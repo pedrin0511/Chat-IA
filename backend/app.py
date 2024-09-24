124d1dc730e13fb443497d8635c2ffd23f1cc797
Pedro Luiz <pedrinholos11@gmail.com>
2024-09-24 09:11:03 -0300

    ajustando chave Api

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Adicione esta linha
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Ativando o cors
 
 # Configurando a API do Gemini
-Api_gemini = 'AIzaSyAYIEfjNZS5qXAqxQgB-ih95X-s99vWlus'
+Api_gemini = os.getenv('API_KEY')
 genai.configure(api_key=Api_gemini)
 model = genai.GenerativeModel("gemini-1.5-pro-latest")
 
